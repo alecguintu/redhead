2657afff8da55995de24b3f616f74bec9435b35c
Alec Guintu <aeguintu@gmail.com>
2012-11-09 16:04:59 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redhead::Application.config.secret_token = '92afc2a5fb1aa33fcb040654a824e1092153aee56b3762ee589a23260a28345d465429091d38eb67ea2ee7cf86d683d076fa6effc1ee4c72ab4ee164dd25b347'
